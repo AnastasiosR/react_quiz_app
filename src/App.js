95a5d6bb5057e95fcabc5630653d9873dfc6219e
Anastasios Rigatos <anastasiosrigatos@192-168-1-105.tpgi.com.au>
2020-12-15 13:29:39 +1100

    Trivia API, Tailwind CSS ADDED

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+//trivia API
+const API_URL = "https://opentdb.com/api.php?amount=10";
+//Tailwind API
+<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"></link>
+function App() {
+  return (
+    <div className="container">
+      This is where we're going to have the question.
+   <div>
+     <div>Answer 1</div>
+     <div>Answer 2</div>
+     <div>Answer 3</div>
+     <div>Answer 4</div>
+   </div>
+    </div>
+  );
+}
+
+export default App;
